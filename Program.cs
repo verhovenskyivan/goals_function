a07691fdd596a7a850f3f862859040588c22e001
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-09 22:40:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class GrassHopper
+{
+    public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+    {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
